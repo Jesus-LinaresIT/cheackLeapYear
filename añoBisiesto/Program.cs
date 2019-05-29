47b34e108c62532a5164753bfaaa726b2faa8d0c
JesusLinares01 <albertolinares001@gmail.com>
2019-05-29 10:01:21 -0600

    cheakLeapYear

---
+++ añoBisiesto/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace añoBisiesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.WriteLine("Digite un año por favor:");
+            a = Convert.ToInt16(Console.ReadLine());
+
+            if (a > 0)
+            {
+                if (a % 4 == 0)
+                {
+                    if (a % 100 == 0)
+                    {
+                        if (a % 400 == 0)
+                        {
+                            Console.WriteLine("Año bisiesto");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Año no bisiesto");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Año bisiesto");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Año no bisiesto");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Año no valido");
+            }
+
+        Console.Write("Disgite Cualquier letra para continuar...");
+            Console.ReadKey();
+
+        }
+    }
+}
